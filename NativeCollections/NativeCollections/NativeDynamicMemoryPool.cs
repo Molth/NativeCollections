9fbe6560cf82b2a2639ac714ce98e9c1a932e64d
Nevin <qq2201591870@163.com>
2024-09-23 21:20:51 +0800

    tostring

--- NativeCollections/NativeCollections/NativeDynamicMemoryPool.cs
+++ NativeCollections/NativeCollections/NativeDynamicMemoryPool.cs
@@ -107,7 +107,7 @@ namespace NativeCollections
         ///     To string
         /// </summary>
         /// <returns>String</returns>
-        public override string ToString() => $"NativeDynamicMemoryPool[{_size}]";
+        public override string ToString() => "NativeDynamicMemoryPool";
 
         /// <summary>
         ///     Equals
