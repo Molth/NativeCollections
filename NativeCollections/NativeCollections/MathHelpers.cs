18e97dda8a0e912a977e94b2943cab1aa3d7dd6a
Molth Nevin <qq2201591870@163.com>
2024-11-18 01:59:25 +0800

    add random

---
+++ NativeCollections/NativeCollections/MathHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+#if NET5_0_OR_GREATER
+using System;
+#endif
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Math helpers
+    /// </summary>
+    internal static class MathHelpers
+    {
+        /// <summary>Produces the full product of two unsigned 64-bit numbers.</summary>
+        /// <param name="a">The first number to multiply.</param>
+        /// <param name="b">The second number to multiply.</param>
+        /// <param name="low">The low 64-bit of the product of the specified numbers.</param>
+        /// <returns>The high 64-bit of the product of the specified numbers.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong BigMul(ulong a, ulong b, out ulong low)
+        {
+#if NET5_0_OR_GREATER
+            return Math.BigMul(a, b, out low);
+#else
+            var al = (uint)a;
+            var ah = (uint)(a >> 32);
+            var bl = (uint)b;
+            var bh = (uint)(b >> 32);
+            var mull = (ulong)al * bl;
+            var t = (ulong)ah * bl + (mull >> 32);
+            var tl = (ulong)al * bh + (uint)t;
+            low = (tl << 32) | (uint)mull;
+            return (ulong)ah * bh + (t >> 32) + (tl >> 32);
+#endif
+        }
+    }
+}
