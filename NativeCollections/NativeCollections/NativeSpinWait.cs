6fda81e05d509e7c8c1e1a311db7f1a9f76d1db6
Molth Nevin <qq2201591870@163.com>
2024-12-13 13:08:56 +0800

    Update NativeSpinWait.cs

--- NativeCollections/NativeCollections/NativeSpinWait.cs
+++ NativeCollections/NativeCollections/NativeSpinWait.cs
@@ -55,6 +55,35 @@ namespace NativeCollections
         }
 
         /// <summary>
+        ///     Spin once
+        /// </summary>
+        /// <param name="sleepThreshold">Sleep threshold</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SpinOnce(int sleepThreshold)
+        {
+            if (sleepThreshold < -1)
+                sleepThreshold = -1;
+            if ((_count >= 10 && ((_count >= sleepThreshold && sleepThreshold >= 0) || (_count - 10) % 2 == 0)) || Environment.ProcessorCount == 1)
+            {
+                if (_count >= sleepThreshold && sleepThreshold >= 0)
+                    Thread.Sleep(1);
+                else if ((_count >= 10 ? (_count - 10) / 2 : _count) % 5 == 4)
+                    Thread.Sleep(0);
+                else
+                    Thread.Yield();
+            }
+            else
+            {
+                var iterations = Environment.ProcessorCount / 2;
+                if (_count <= 30 && 1 << _count < iterations)
+                    iterations = 1 << _count;
+                Thread.SpinWait(iterations);
+            }
+
+            _count = _count == int.MaxValue ? 10 : _count + 1;
+        }
+
+        /// <summary>
         ///     Empty
         /// </summary>
         public static NativeSpinWait Empty => new();
