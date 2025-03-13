62735364f843215a061cb046fdcf1c699712b0a8
Molth Nevin <qq2201591870@163.com>
2025-03-13 21:31:31 +0800

    rename

---
+++ NativeCollections/NativeCollections/NativeMemoryPool`1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native memory pool
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection]
+    public readonly unsafe struct NativeMemoryPool<T> : IDisposable, IEquatable<NativeMemoryPool<T>> where T : unmanaged
+    {
+        /// <summary>
+        ///     Handle
+        /// </summary>
+        private readonly NativeMemoryPool _handle;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="size">Size</param>
+        /// <param name="maxFreeSlabs">Max free slabs</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeMemoryPool(int size, int maxFreeSlabs) => _handle = new NativeMemoryPool(size, sizeof(T), maxFreeSlabs);
+
+        /// <summary>
+        ///     Is created
+        /// </summary>
+        public bool IsCreated => _handle.IsCreated;
+
+        /// <summary>
+        ///     Slabs
+        /// </summary>
+        public int Slabs => _handle.Slabs;
+
+        /// <summary>
+        ///     Free slabs
+        /// </summary>
+        public int FreeSlabs => _handle.FreeSlabs;
+
+        /// <summary>
+        ///     Max free slabs
+        /// </summary>
+        public int MaxFreeSlabs => _handle.MaxFreeSlabs;
+
+        /// <summary>
+        ///     Size
+        /// </summary>
+        public int Size => _handle.Size;
+
+        /// <summary>
+        ///     Length
+        /// </summary>
+        public int Length => _handle.Length;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Equals</returns>
+        public bool Equals(NativeMemoryPool<T> other) => other == this;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <returns>Equals</returns>
+        public override bool Equals(object? obj) => obj is NativeMemoryPool<T> nativeMemoryPool && nativeMemoryPool == this;
+
+        /// <summary>
+        ///     Get hashCode
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode() => _handle.GetHashCode();
+
+        /// <summary>
+        ///     To string
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => $"NativeMemoryPool<{typeof(T).Name}>";
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Equals</returns>
+        public static bool operator ==(NativeMemoryPool<T> left, NativeMemoryPool<T> right) => left._handle == right._handle;
+
+        /// <summary>
+        ///     Not equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Not equals</returns>
+        public static bool operator !=(NativeMemoryPool<T> left, NativeMemoryPool<T> right) => left._handle != right._handle;
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() => _handle.Dispose();
+
+        /// <summary>
+        ///     Rent buffer
+        /// </summary>
+        /// <returns>Buffer</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T* Rent() => (T*)_handle.Rent();
+
+        /// <summary>
+        ///     Return buffer
+        /// </summary>
+        /// <param name="ptr">Pointer</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Return(T* ptr) => _handle.Return(ptr);
+
+        /// <summary>
+        ///     Ensure capacity
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        /// <returns>New capacity</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int EnsureCapacity(int capacity) => _handle.EnsureCapacity(capacity);
+
+        /// <summary>
+        ///     Trim excess
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void TrimExcess() => _handle.TrimExcess();
+
+        /// <summary>
+        ///     Trim excess
+        /// </summary>
+        /// <param name="capacity">Remaining free slabs</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int TrimExcess(int capacity) => _handle.TrimExcess(capacity);
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static NativeMemoryPool<T> Empty => new();
+    }
+}
---
+++ NativeCollections/NativeCollections/NativeObject`1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8600
+#pragma warning disable CS8603
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native object
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection]
+    public struct NativeObject<T> : IDisposable, IEquatable<NativeObject<T>>
+    {
+        /// <summary>
+        ///     Handle
+        /// </summary>
+        private GCHandle _handle;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="handle">GCHandle</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeObject(GCHandle handle) => _handle = handle;
+
+        /// <summary>
+        ///     Is created
+        /// </summary>
+        public bool IsCreated => _handle.IsAllocated;
+
+        /// <summary>
+        ///     Value
+        /// </summary>
+        public T Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (T)_handle.Target;
+        }
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Equals</returns>
+        public bool Equals(NativeObject<T> other) => other == this;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <returns>Equals</returns>
+        public override bool Equals(object? obj) => obj is NativeObject<T> nativeObject && nativeObject == this;
+
+        /// <summary>
+        ///     Get hashCode
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode() => ((nint)_handle).GetHashCode();
+
+        /// <summary>
+        ///     To string
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => "NativeObject";
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Equals</returns>
+        public static bool operator ==(NativeObject<T> left, NativeObject<T> right) => left._handle == right._handle;
+
+        /// <summary>
+        ///     Not equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Not equals</returns>
+        public static bool operator !=(NativeObject<T> left, NativeObject<T> right) => left._handle != right._handle;
+
+        /// <summary>
+        ///     As value
+        /// </summary>
+        /// <returns>Value</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator T(NativeObject<T> nativeObject) => nativeObject.Value;
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            var handle = _handle;
+            if (!handle.IsAllocated)
+                return;
+            handle.Free();
+        }
+
+        /// <summary>
+        ///     Alloc
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <returns>NativeObject</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static NativeObject<T> Alloc(object? value) => new(GCHandle.Alloc(value));
+
+        /// <summary>
+        ///     Alloc
+        /// </summary>
+        /// <param name="value">Value</param>
+        /// <param name="type">Type</param>
+        /// <returns>NativeObject</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static NativeObject<T> Alloc(object? value, GCHandleType type) => new(GCHandle.Alloc(value, type));
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static NativeObject<T> Empty => new();
+    }
+}
