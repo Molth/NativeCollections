458d587932d1d078441b33e8872d82cdd9b94b12
Molth Nevin <qq2201591870@163.com>
2025-03-24 12:12:36 +0800

    Create NativeLinkedListNode.cs

---
+++ NativeCollections/NativeCollections/NativeLinkedListNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native linked list node
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection(NativeCollectionType.None)]
+    public unsafe struct NativeLinkedListNode
+    {
+        /// <summary>
+        ///     Next
+        /// </summary>
+        public NativeLinkedListNode* Next;
+
+        /// <summary>
+        ///     Previous
+        /// </summary>
+        public NativeLinkedListNode* Previous;
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static NativeLinkedListNode Empty => new();
+    }
+}
