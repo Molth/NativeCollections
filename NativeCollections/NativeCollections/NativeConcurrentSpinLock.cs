2e35bf63481007520498fcfb8e68b758c5c16730
Molth Nevin <qq2201591870@163.com>
2025-03-25 18:08:20 +0800

    Update NativeConcurrentSpinLock.cs

--- NativeCollections/NativeCollections/NativeConcurrentSpinLock.cs
+++ NativeCollections/NativeCollections/NativeConcurrentSpinLock.cs
@@ -32,6 +32,79 @@ namespace NativeCollections
             ///     Next sequence number
             /// </summary>
             [FieldOffset(4)] public volatile int NextSequenceNumber;
+
+            /// <summary>
+            ///     Reset
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Reset()
+            {
+                SequenceNumber = 0;
+                NextSequenceNumber = 1;
+            }
+
+            /// <summary>
+            ///     Acquire
+            /// </summary>
+            /// <returns>Sequence number</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public int Acquire() => Interlocked.Add(ref SequenceNumber, 1);
+
+            /// <summary>
+            ///     Wait
+            /// </summary>
+            /// <param name="sequenceNumber">Sequence number</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Wait(int sequenceNumber)
+            {
+                var spinWait = new NativeSpinWait();
+                while (sequenceNumber != NextSequenceNumber)
+                    spinWait.SpinOnce();
+            }
+
+            /// <summary>
+            ///     Wait
+            /// </summary>
+            /// <param name="sequenceNumber">Sequence number</param>
+            /// <param name="sleepThreshold">Sleep threshold</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Wait(int sequenceNumber, int sleepThreshold)
+            {
+                var spinWait = new NativeSpinWait();
+                while (sequenceNumber != NextSequenceNumber)
+                    spinWait.SpinOnce(sleepThreshold);
+            }
+
+            /// <summary>
+            ///     Enter
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Enter()
+            {
+                var spinWait = new NativeSpinWait();
+                var sequenceNumber = Interlocked.Add(ref SequenceNumber, 1);
+                while (sequenceNumber != NextSequenceNumber)
+                    spinWait.SpinOnce();
+            }
+
+            /// <summary>
+            ///     Enter
+            /// </summary>
+            /// <param name="sleepThreshold">Sleep threshold</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Enter(int sleepThreshold)
+            {
+                var spinWait = new NativeSpinWait();
+                var sequenceNumber = Interlocked.Add(ref SequenceNumber, 1);
+                while (sequenceNumber != NextSequenceNumber)
+                    spinWait.SpinOnce(sleepThreshold);
+            }
+
+            /// <summary>
+            ///     Exit
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void Exit() => Interlocked.Add(ref NextSequenceNumber, 1);
         }
 
         /// <summary>
@@ -107,32 +180,21 @@ namespace NativeCollections
         ///     Reset
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Reset()
-        {
-            var handle = _handle;
-            handle->SequenceNumber = 0;
-            handle->NextSequenceNumber = 1;
-        }
+        public void Reset() => _handle->Reset();
 
         /// <summary>
         ///     Acquire
         /// </summary>
         /// <returns>Sequence number</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int Acquire() => Interlocked.Add(ref _handle->SequenceNumber, 1);
+        public int Acquire() => _handle->Acquire();
 
         /// <summary>
         ///     Wait
         /// </summary>
         /// <param name="sequenceNumber">Sequence number</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Wait(int sequenceNumber)
-        {
-            var handle = _handle;
-            var spinWait = new NativeSpinWait();
-            while (sequenceNumber != handle->NextSequenceNumber)
-                spinWait.SpinOnce();
-        }
+        public void Wait(int sequenceNumber) => _handle->Wait(sequenceNumber);
 
         /// <summary>
         ///     Wait
@@ -140,46 +202,26 @@ namespace NativeCollections
         /// <param name="sequenceNumber">Sequence number</param>
         /// <param name="sleepThreshold">Sleep threshold</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Wait(int sequenceNumber, int sleepThreshold)
-        {
-            var handle = _handle;
-            var spinWait = new NativeSpinWait();
-            while (sequenceNumber != handle->NextSequenceNumber)
-                spinWait.SpinOnce(sleepThreshold);
-        }
+        public void Wait(int sequenceNumber, int sleepThreshold) => _handle->Wait(sequenceNumber, sleepThreshold);
 
         /// <summary>
         ///     Enter
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Enter()
-        {
-            var handle = _handle;
-            var spinWait = new NativeSpinWait();
-            var sequenceNumber = Interlocked.Add(ref handle->SequenceNumber, 1);
-            while (sequenceNumber != handle->NextSequenceNumber)
-                spinWait.SpinOnce();
-        }
+        public void Enter() => _handle->Enter();
 
         /// <summary>
         ///     Enter
         /// </summary>
         /// <param name="sleepThreshold">Sleep threshold</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Enter(int sleepThreshold)
-        {
-            var handle = _handle;
-            var spinWait = new NativeSpinWait();
-            var sequenceNumber = Interlocked.Add(ref handle->SequenceNumber, 1);
-            while (sequenceNumber != handle->NextSequenceNumber)
-                spinWait.SpinOnce(sleepThreshold);
-        }
+        public void Enter(int sleepThreshold) => _handle->Enter(sleepThreshold);
 
         /// <summary>
         ///     Exit
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Exit() => Interlocked.Add(ref _handle->NextSequenceNumber, 1);
+        public void Exit() => _handle->Exit();
 
         /// <summary>
         ///     As native concurrent spinLock
