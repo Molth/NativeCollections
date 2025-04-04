7cc5201bc1fc386b046ac64e0cb297202cf67a1e
Molth Nevin <qq2201591870@163.com>
2025-04-04 13:33:22 +0800

    create SortedList`1

---
+++ NativeCollections/NativeCollections/NativeSortedList`1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native sortedList
+    /// </summary>
+    /// <typeparam name="TKey">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection(FromType.Standard)]
+    [BindingType(typeof(UnsafeSortedList<>))]
+    public readonly unsafe struct NativeSortedList<TKey> where TKey : unmanaged, IComparable<TKey>
+    {
+        /// <summary>
+        ///     Handle
+        /// </summary>
+        private readonly UnsafeSortedList<TKey>* _handle;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeSortedList(int capacity)
+        {
+            var value = new UnsafeSortedList<TKey>(capacity);
+            var handle = (UnsafeSortedList<TKey>*)NativeMemoryAllocator.Alloc((uint)sizeof(UnsafeSortedList<TKey>));
+            *handle = value;
+            _handle = handle;
+        }
+
+        /// <summary>
+        ///     Is created
+        /// </summary>
+        public bool IsCreated => _handle != null;
+
+        /// <summary>
+        ///     Is empty
+        /// </summary>
+        public bool IsEmpty => _handle->IsEmpty;
+
+        /// <summary>
+        ///     Count
+        /// </summary>
+        public int Count => _handle->Count;
+
+        /// <summary>
+        ///     Capacity
+        /// </summary>
+        public int Capacity => _handle->Capacity;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Equals</returns>
+        public bool Equals(NativeSortedList<TKey> other) => other == this;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <returns>Equals</returns>
+        public override bool Equals(object? obj) => obj is NativeSortedList<TKey> nativeSortedList && nativeSortedList == this;
+
+        /// <summary>
+        ///     Get hashCode
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode() => ((nint)_handle).GetHashCode();
+
+        /// <summary>
+        ///     To string
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => $"NativeSortedList<{typeof(TKey).Name}>";
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Equals</returns>
+        public static bool operator ==(NativeSortedList<TKey> left, NativeSortedList<TKey> right) => left._handle == right._handle;
+
+        /// <summary>
+        ///     Not equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Not equals</returns>
+        public static bool operator !=(NativeSortedList<TKey> left, NativeSortedList<TKey> right) => left._handle != right._handle;
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            var handle = _handle;
+            if (handle == null)
+                return;
+            handle->Dispose();
+            NativeMemoryAllocator.Free(handle);
+        }
+
+        /// <summary>
+        ///     Clear
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear() => _handle->Clear();
+
+        /// <summary>
+        ///     Add
+        /// </summary>
+        /// <param name="key">Key</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(in TKey key) => _handle->Add(key);
+
+        /// <summary>
+        ///     Remove
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Removed</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Remove(in TKey key) => _handle->Remove(key);
+
+        /// <summary>
+        ///     Remove at
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Removed</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RemoveAt(int index) => _handle->RemoveAt(index);
+
+        /// <summary>
+        ///     Remove range
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <param name="count">Count</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RemoveRange(int index, int count) => _handle->RemoveRange(index, count);
+
+        /// <summary>
+        ///     Get key at index
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Key</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TKey GetKeyAtIndex(int index) => _handle->GetKeyAtIndex(index);
+
+        /// <summary>
+        ///     Contains key
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Contains key</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool ContainsKey(in TKey key) => _handle->ContainsKey(key);
+
+        /// <summary>
+        ///     Ensure capacity
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        /// <returns>New capacity</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int EnsureCapacity(int capacity) => _handle->EnsureCapacity(capacity);
+
+        /// <summary>
+        ///     Trim excess
+        /// </summary>
+        /// <returns>New capacity</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int TrimExcess() => _handle->TrimExcess();
+
+        /// <summary>
+        ///     Set capacity
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetCapacity(int capacity) => _handle->SetCapacity(capacity);
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static NativeSortedList<TKey> Empty => new();
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        /// <returns>Enumerator</returns>
+        public UnsafeSortedList<TKey>.Enumerator GetEnumerator() => _handle->GetEnumerator();
+    }
+}
---
+++ NativeCollections/NativeCollections/UnsafeSortedList`1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Unsafe sortedList
+    /// </summary>
+    /// <typeparam name="TKey">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [UnsafeCollection(FromType.Standard)]
+    public unsafe struct UnsafeSortedList<TKey> : IDisposable where TKey : unmanaged, IComparable<TKey>
+    {
+        /// <summary>
+        ///     Keys
+        /// </summary>
+        private TKey* _keys;
+
+        /// <summary>
+        ///     Size
+        /// </summary>
+        private int _size;
+
+        /// <summary>
+        ///     Version
+        /// </summary>
+        private int _version;
+
+        /// <summary>
+        ///     Capacity
+        /// </summary>
+        private int _capacity;
+
+        /// <summary>
+        ///     Is empty
+        /// </summary>
+        public bool IsEmpty => _size == 0;
+
+        /// <summary>
+        ///     Count
+        /// </summary>
+        public int Count => _size;
+
+        /// <summary>
+        ///     Capacity
+        /// </summary>
+        public int Capacity => _capacity;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UnsafeSortedList(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), capacity, "MustBeNonNegative");
+            if (capacity < 4)
+                capacity = 4;
+            _keys = (TKey*)NativeMemoryAllocator.Alloc((uint)(capacity * sizeof(TKey)));
+            _size = 0;
+            _version = 0;
+            _capacity = capacity;
+        }
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() => NativeMemoryAllocator.Free(_keys);
+
+        /// <summary>
+        ///     Clear
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear()
+        {
+            ++_version;
+            _size = 0;
+        }
+
+        /// <summary>
+        ///     Add
+        /// </summary>
+        /// <param name="key">Key</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Add(in TKey key)
+        {
+            var num = BinarySearchHelpers.IndexOf(_keys, _size, key);
+            if (num >= 0)
+                throw new ArgumentException($"AddingDuplicate, {key}", nameof(key));
+            Insert(~num, key);
+        }
+
+        /// <summary>
+        ///     Remove
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Removed</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Remove(in TKey key)
+        {
+            var index = BinarySearchHelpers.IndexOf(_keys, _size, key);
+            if (index >= 0)
+            {
+                --_size;
+                if (index < _size)
+                    Unsafe.CopyBlockUnaligned(_keys + index, _keys + index + 1, (uint)((_size - index) * sizeof(TKey)));
+                ++_version;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///     Remove at
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Removed</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RemoveAt(int index)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "MustBeNonNegative");
+            if (index >= _size)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "IndexMustBeLess");
+            --_size;
+            if (index < _size)
+                Unsafe.CopyBlockUnaligned(_keys + index, _keys + index + 1, (uint)((_size - index) * sizeof(TKey)));
+            ++_version;
+        }
+
+        /// <summary>
+        ///     Remove range
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <param name="count">Count</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void RemoveRange(int index, int count)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "MustBeNonNegative");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count), count, "MustBeNonNegative");
+            if (count == 0)
+                return;
+            if (index + count > _size)
+                throw new ArgumentOutOfRangeException(nameof(count), count, "MustBeLess");
+            _size -= count;
+            if (index < _size)
+                Unsafe.CopyBlockUnaligned(_keys + index, _keys + index + count, (uint)((_size - index) * sizeof(TKey)));
+            ++_version;
+        }
+
+        /// <summary>
+        ///     Get key at index
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <returns>Key</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public TKey GetKeyAtIndex(int index)
+        {
+            if (index < 0)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "MustBeNonNegative");
+            if (index >= _size)
+                throw new ArgumentOutOfRangeException(nameof(index), index, "IndexMustBeLess");
+            return _keys[index];
+        }
+
+        /// <summary>
+        ///     Contains key
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Contains key</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool ContainsKey(in TKey key) => BinarySearchHelpers.IndexOf(_keys, _size, key) >= 0;
+
+        /// <summary>
+        ///     Ensure capacity
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        /// <returns>New capacity</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int EnsureCapacity(int capacity)
+        {
+            if (_capacity < capacity)
+            {
+                var newCapacity = 2 * _capacity;
+                if ((uint)newCapacity > 2147483591)
+                    newCapacity = 2147483591;
+                var expected = _capacity + 4;
+                newCapacity = newCapacity > expected ? newCapacity : expected;
+                if (newCapacity < capacity)
+                    newCapacity = capacity;
+                SetCapacity(newCapacity);
+            }
+
+            return _capacity;
+        }
+
+        /// <summary>
+        ///     Trim excess
+        /// </summary>
+        /// <returns>New capacity</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int TrimExcess()
+        {
+            var threshold = (int)(_capacity * 0.9);
+            if (_size < threshold)
+                SetCapacity(_size);
+            return _capacity;
+        }
+
+        /// <summary>
+        ///     Set capacity
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetCapacity(int capacity)
+        {
+            if (capacity < _size)
+                throw new ArgumentOutOfRangeException(nameof(capacity), capacity, "Small_Capacity");
+            if (capacity != _capacity)
+            {
+                if (capacity > 0)
+                {
+                    var keys = (TKey*)NativeMemoryAllocator.Alloc((uint)(capacity * sizeof(TKey)));
+                    if (_size > 0)
+                    {
+                        Unsafe.CopyBlockUnaligned(keys, _keys, (uint)(_size * sizeof(TKey)));
+                        NativeMemoryAllocator.Free(_keys);
+                    }
+
+                    _keys = keys;
+                }
+                else
+                {
+                    NativeMemoryAllocator.Free(_keys);
+                    _keys = null;
+                }
+
+                _capacity = capacity;
+            }
+        }
+
+        /// <summary>
+        ///     Insert
+        /// </summary>
+        /// <param name="index">Index</param>
+        /// <param name="key">Key</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Insert(int index, in TKey key)
+        {
+            if (_size == _capacity)
+                EnsureCapacity(_size + 1);
+            if (index < _size)
+                Unsafe.CopyBlockUnaligned(_keys + index + 1, _keys + index, (uint)((_size - index) * sizeof(TKey)));
+            _keys[index] = key;
+            ++_size;
+            ++_version;
+        }
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static UnsafeSortedList<TKey> Empty => new();
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        /// <returns>Enumerator</returns>
+        public Enumerator GetEnumerator() => new(Unsafe.AsPointer(ref this));
+
+        /// <summary>
+        ///     Enumerator
+        /// </summary>
+        public struct Enumerator
+        {
+            /// <summary>
+            ///     NativeSortedList
+            /// </summary>
+            private readonly UnsafeSortedList<TKey>* _nativeSortedList;
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            private TKey _current;
+
+            /// <summary>
+            ///     Index
+            /// </summary>
+            private int _index;
+
+            /// <summary>
+            ///     Version
+            /// </summary>
+            private readonly int _version;
+
+            /// <summary>
+            ///     Structure
+            /// </summary>
+            /// <param name="nativeSortedList">NativeSortedList</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(void* nativeSortedList)
+            {
+                var handle = (UnsafeSortedList<TKey>*)nativeSortedList;
+                _nativeSortedList = handle;
+                _current = default;
+                _index = 0;
+                _version = handle->_version;
+            }
+
+            /// <summary>
+            ///     Move next
+            /// </summary>
+            /// <returns>Moved</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                var handle = _nativeSortedList;
+                if (_version != handle->_version)
+                    throw new InvalidOperationException("EnumFailedVersion");
+                if ((uint)_index < (uint)handle->_size)
+                {
+                    _current = handle->_keys[_index];
+                    ++_index;
+                    return true;
+                }
+
+                _index = handle->_size + 1;
+                return false;
+            }
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            public TKey Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _current;
+            }
+        }
+    }
+}
