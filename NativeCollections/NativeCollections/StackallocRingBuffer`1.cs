6bf9bfa26d58666f7041d429a3fbe846ec27e399
Molth Nevin <qq2201591870@163.com>
2025-04-14 13:31:51 +0800

    Create StackallocRingBuffer`1.cs

---
+++ NativeCollections/NativeCollections/StackallocRingBuffer`1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Stackalloc ringBuffer
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [StackallocCollection(FromType.None)]
+    public unsafe struct StackallocRingBuffer<T> where T : unmanaged
+    {
+        /// <summary>
+        ///     Buffer
+        /// </summary>
+        private readonly T* _buffer;
+
+        /// <summary>
+        ///     Length
+        /// </summary>
+        private readonly int _length;
+
+        /// <summary>
+        ///     Head
+        /// </summary>
+        private int _head;
+
+        /// <summary>
+        ///     Tail
+        /// </summary>
+        private int _tail;
+
+        /// <summary>
+        ///     Size
+        /// </summary>
+        private int _size;
+
+        /// <summary>
+        ///     Version
+        /// </summary>
+        private int _version;
+
+        /// <summary>
+        ///     Is empty
+        /// </summary>
+        public bool IsEmpty => _size == 0;
+
+        /// <summary>
+        ///     Count
+        /// </summary>
+        public int Count => _size;
+
+        /// <summary>
+        ///     Capacity
+        /// </summary>
+        public int Capacity => _length;
+
+        /// <summary>
+        ///     Get reference
+        /// </summary>
+        /// <param name="index">Index</param>
+        public ref T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref _buffer[(_head + index) % _length];
+        }
+
+        /// <summary>
+        ///     Get reference
+        /// </summary>
+        /// <param name="index">Index</param>
+        public ref T this[uint index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref _buffer[(_head + index) % _length];
+        }
+
+        /// <summary>
+        ///     Get byte count
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        /// <returns>Byte count</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetByteCount(int capacity) => capacity * sizeof(T);
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public StackallocRingBuffer(Span<byte> buffer, int capacity)
+        {
+            _buffer = (T*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer));
+            _length = capacity;
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+            _version = 0;
+        }
+
+        /// <summary>
+        ///     Clear
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear()
+        {
+            _size = 0;
+            _head = 0;
+            _tail = 0;
+            _version++;
+        }
+
+        /// <summary>
+        ///     Enqueue head
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Overwritten</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool EnqueueHead(in T item)
+        {
+            bool overwritten;
+            if (_size == _length)
+            {
+                if (--_tail == -1)
+                    _tail = _length - 1;
+                overwritten = true;
+            }
+            else
+            {
+                ++_size;
+                overwritten = false;
+            }
+
+            if (--_head == -1)
+                _head = _length - 1;
+            _buffer[_head] = item;
+            ++_version;
+            return overwritten;
+        }
+
+        /// <summary>
+        ///     Try enqueue head
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Enqueued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryEnqueueHead(in T item)
+        {
+            if (_size == _length)
+                return false;
+            if (--_head == -1)
+                _head = _length - 1;
+            _buffer[_head] = item;
+            ++_size;
+            ++_version;
+            return true;
+        }
+
+        /// <summary>
+        ///     Enqueue tail
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Overwritten</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool EnqueueTail(in T item)
+        {
+            bool overwritten;
+            if (_size == _length)
+            {
+                if (++_head == _length)
+                    _head = 0;
+                overwritten = true;
+            }
+            else
+            {
+                ++_size;
+                overwritten = false;
+            }
+
+            _buffer[_tail] = item;
+            if (++_tail == _length)
+                _tail = 0;
+            ++_version;
+            return overwritten;
+        }
+
+        /// <summary>
+        ///     Try enqueue tail
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Enqueued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryEnqueueTail(in T item)
+        {
+            if (_size == _length)
+                return false;
+            _buffer[_tail] = item;
+            if (++_tail == _length)
+                _tail = 0;
+            ++_size;
+            ++_version;
+            return true;
+        }
+
+        /// <summary>
+        ///     Try dequeue
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Dequeued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryDequeueHead(out T result)
+        {
+            if (_size == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = _buffer[_head];
+            if (++_head == _length)
+                _head = 0;
+            --_size;
+            ++_version;
+            return true;
+        }
+
+        /// <summary>
+        ///     Try dequeue
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Dequeued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryDequeueTail(out T result)
+        {
+            if (_size == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            if (--_tail == -1)
+                _tail = _length - 1;
+            result = _buffer[_tail];
+            --_size;
+            ++_version;
+            return true;
+        }
+
+        /// <summary>
+        ///     Try peek head
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Peeked</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeekHead(out T result)
+        {
+            if (_size == 0)
+            {
+                result = default;
+                return false;
+            }
+
+            result = _buffer[_head];
+            return true;
+        }
+
+        /// <summary>
+        ///     Try peek tail
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Peeked</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeekTail(out T result)
+        {
+            var size = _size - 1;
+            if ((uint)size >= (uint)_length)
+            {
+                result = default;
+                return false;
+            }
+
+            result = _buffer[size];
+            return true;
+        }
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static StackallocRingBuffer<T> Empty => new();
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        /// <returns>Enumerator</returns>
+        public Enumerator GetEnumerator() => new(Unsafe.AsPointer(ref this));
+
+        /// <summary>
+        ///     Enumerator
+        /// </summary>
+        public struct Enumerator
+        {
+            /// <summary>
+            ///     NativeDeque
+            /// </summary>
+            private readonly StackallocRingBuffer<T>* _nativeDeque;
+
+            /// <summary>
+            ///     Version
+            /// </summary>
+            private readonly int _version;
+
+            /// <summary>
+            ///     Index
+            /// </summary>
+            private int _index;
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            private T _currentElement;
+
+            /// <summary>
+            ///     Structure
+            /// </summary>
+            /// <param name="nativeDeque">NativeDeque</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(void* nativeDeque)
+            {
+                var handle = (StackallocRingBuffer<T>*)nativeDeque;
+                _nativeDeque = handle;
+                _version = handle->_version;
+                _index = -1;
+                _currentElement = default;
+            }
+
+            /// <summary>
+            ///     Move next
+            /// </summary>
+            /// <returns>Moved</returns>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                var handle = _nativeDeque;
+                if (_version != handle->_version)
+                    throw new InvalidOperationException("EnumFailedVersion");
+                if (_index == -2)
+                    return false;
+                _index++;
+                if (_index == handle->_size)
+                {
+                    _index = -2;
+                    _currentElement = default;
+                    return false;
+                }
+
+                var buffer = handle->_buffer;
+                var capacity = (uint)handle->_length;
+                var index = (uint)(handle->_head + _index);
+                if (index >= capacity)
+                    index -= capacity;
+                _currentElement = buffer[index];
+                return true;
+            }
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            public T Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _index < 0 ? throw new InvalidOperationException(_index == -1 ? "EnumNotStarted" : "EnumEnded") : _currentElement;
+            }
+        }
+    }
+}
