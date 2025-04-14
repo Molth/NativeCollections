9d29153886f6d7454b303b02869cf3a6b1cbb742
Molth Nevin <qq2201591870@163.com>
2025-04-14 13:24:32 +0800

    RingBuffer

---
+++ NativeCollections/NativeCollections/NativeRingBuffer`1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native ringBuffer
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection(FromType.None)]
+    [BindingType(typeof(UnsafeRingBuffer<>))]
+    public readonly unsafe struct NativeRingBuffer<T> : IDisposable, IEquatable<NativeRingBuffer<T>> where T : unmanaged
+    {
+        /// <summary>
+        ///     Handle
+        /// </summary>
+        private readonly UnsafeRingBuffer<T>* _handle;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="capacity">Capacity</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeRingBuffer(int capacity)
+        {
+            var value = new UnsafeRingBuffer<T>(capacity);
+            var handle = (UnsafeRingBuffer<T>*)NativeMemoryAllocator.Alloc((uint)sizeof(UnsafeRingBuffer<T>));
+            *handle = value;
+            _handle = handle;
+        }
+
+        /// <summary>
+        ///     Is created
+        /// </summary>
+        public bool IsCreated => _handle != null;
+
+        /// <summary>
+        ///     Is empty
+        /// </summary>
+        public bool IsEmpty => _handle->IsEmpty;
+
+        /// <summary>
+        ///     Count
+        /// </summary>
+        public int Count => _handle->Count;
+
+        /// <summary>
+        ///     Capacity
+        /// </summary>
+        public int Capacity => _handle->Capacity;
+
+        /// <summary>
+        ///     Get reference
+        /// </summary>
+        /// <param name="index">Index</param>
+        public ref T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref Unsafe.AsRef<UnsafeRingBuffer<T>>(_handle)[index];
+        }
+
+        /// <summary>
+        ///     Get reference
+        /// </summary>
+        /// <param name="index">Index</param>
+        public ref T this[uint index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => ref Unsafe.AsRef<UnsafeRingBuffer<T>>(_handle)[index];
+        }
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="other">Other</param>
+        /// <returns>Equals</returns>
+        public bool Equals(NativeRingBuffer<T> other) => other == this;
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="obj">object</param>
+        /// <returns>Equals</returns>
+        public override bool Equals(object? obj) => obj is NativeRingBuffer<T> nativeRingBuffer && nativeRingBuffer == this;
+
+        /// <summary>
+        ///     Get hashCode
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode() => ((nint)_handle).GetHashCode();
+
+        /// <summary>
+        ///     To string
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => $"NativeRingBuffer<{typeof(T).Name}>";
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Equals</returns>
+        public static bool operator ==(NativeRingBuffer<T> left, NativeRingBuffer<T> right) => left._handle == right._handle;
+
+        /// <summary>
+        ///     Not equals
+        /// </summary>
+        /// <param name="left">Left</param>
+        /// <param name="right">Right</param>
+        /// <returns>Not equals</returns>
+        public static bool operator !=(NativeRingBuffer<T> left, NativeRingBuffer<T> right) => left._handle != right._handle;
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+            var handle = _handle;
+            if (handle == null)
+                return;
+            handle->Dispose();
+            NativeMemoryAllocator.Free(handle);
+        }
+
+        /// <summary>
+        ///     Clear
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Clear() => _handle->Clear();
+
+        /// <summary>
+        ///     Enqueue head
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Overwritten</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool EnqueueHead(in T item) => _handle->EnqueueHead(item);
+
+        /// <summary>
+        ///     Try enqueue head
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Enqueued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryEnqueueHead(in T item) => _handle->TryEnqueueHead(item);
+
+        /// <summary>
+        ///     Enqueue tail
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Overwritten</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool EnqueueTail(in T item) => _handle->EnqueueTail(item);
+
+        /// <summary>
+        ///     Try enqueue tail
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Enqueued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryEnqueueTail(in T item) => _handle->TryEnqueueTail(item);
+
+        /// <summary>
+        ///     Try dequeue
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Dequeued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryDequeueHead(out T result) => _handle->TryDequeueHead(out result);
+
+        /// <summary>
+        ///     Try dequeue
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Dequeued</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryDequeueTail(out T result) => _handle->TryDequeueTail(out result);
+
+        /// <summary>
+        ///     Try peek head
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Peeked</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeekHead(out T result) => _handle->TryPeekHead(out result);
+
+        /// <summary>
+        ///     Try peek tail
+        /// </summary>
+        /// <param name="result">Item</param>
+        /// <returns>Peeked</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryPeekTail(out T result) => _handle->TryPeekTail(out result);
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static NativeRingBuffer<T> Empty => new();
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        /// <returns>Enumerator</returns>
+        public UnsafeRingBuffer<T>.Enumerator GetEnumerator() => _handle->GetEnumerator();
+    }
+}
