c3758426dbd280f2818a345042a37a6139c1bd6d
Molth Nevin <qq2201591870@163.com>
2025-05-21 12:47:12 +0800

    Update NativeString.cs

--- NativeCollections/NativeCollections/NativeString.cs
+++ NativeCollections/NativeCollections/NativeString.cs
@@ -997,6 +997,11 @@ namespace NativeCollections
         public static ReadOnlySpan<byte> NewLineUtf8 => _NewLineUtf8;
 
         /// <summary>
+        ///     New line chars
+        /// </summary>
+        public static ReadOnlySpan<char> NewLineChars => "\r\f\u0085\u2028\u2029\n";
+
+        /// <summary>
         ///     WhiteSpace chars
         /// </summary>
         public static ReadOnlySpan<char> WhiteSpaceChars => "\t\n\v\f\r\u0020\u0085\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000";
