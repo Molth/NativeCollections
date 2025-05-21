ba7f5c9e6bfa20b5e0af31de50696d4210ee4c20
Molth Nevin <qq2201591870@163.com>
2025-05-21 12:15:55 +0800

    Split

---
+++ NativeCollections/NativeCollections/NativeSplitRange`1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native split range
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection(FromType.None)]
+    public readonly unsafe ref struct NativeSplitRange<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        ///     Buffer
+        /// </summary>
+        private readonly ReadOnlySpan<T> _buffer;
+
+        /// <summary>
+        ///     Separator
+        /// </summary>
+        private readonly ReadOnlySpan<T> _separator;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="separator">Separator</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeSplitRange(ReadOnlySpan<T> buffer, in T separator)
+        {
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer)) == null || buffer.Length == 0)
+                throw new ArgumentNullException(nameof(buffer));
+            if (separator == null)
+                throw new ArgumentNullException(nameof(separator));
+            _buffer = buffer;
+            _separator = MemoryMarshal.CreateReadOnlySpan(ref Unsafe.AsRef(in separator), 1);
+        }
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="separator">Separator</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeSplitRange(ReadOnlySpan<T> buffer, ReadOnlySpan<T> separator)
+        {
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer)) == null || buffer.Length == 0)
+                throw new ArgumentNullException(nameof(buffer));
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(separator)) == null || separator.Length == 0)
+                throw new ArgumentNullException(nameof(separator));
+            _buffer = buffer;
+            _separator = separator;
+        }
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        public Enumerator GetEnumerator() => new(_buffer, _separator);
+
+        /// <summary>
+        ///     Enumerator
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public ref struct Enumerator
+        {
+            /// <summary>
+            ///     Current
+            /// </summary>
+            private Range _current;
+
+            /// <summary>
+            ///     Next
+            /// </summary>
+            private int _next;
+
+            /// <summary>
+            ///     Buffer
+            /// </summary>
+            private readonly ReadOnlySpan<T> _buffer;
+
+            /// <summary>
+            ///     Separator
+            /// </summary>
+            private readonly ReadOnlySpan<T> _separator;
+
+            /// <summary>
+            ///     Structure
+            /// </summary>
+            /// <param name="buffer">Buffer</param>
+            /// <param name="separator">Separator</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(ReadOnlySpan<T> buffer, ReadOnlySpan<T> separator)
+            {
+                _current = default;
+                _next = 0;
+                _buffer = buffer;
+                _separator = separator;
+            }
+
+            /// <summary>
+            ///     Move next
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                var buffer = _buffer.Slice(_next);
+                var index = _separator.Length == 1 ? buffer.IndexOf(_separator[0]) : buffer.IndexOf(_separator);
+                if (index < 0)
+                {
+                    if (buffer.Length > 0)
+                    {
+                        _current = new Range(_next, _next + buffer.Length);
+                        _next = _buffer.Length;
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                _current = new Range(_next, _next + index);
+                _next += index + _separator.Length;
+                return true;
+            }
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            public Range Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _current;
+            }
+        }
+    }
+}
---
+++ NativeCollections/NativeCollections/NativeSplit`1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native split
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [NativeCollection(FromType.None)]
+    public readonly unsafe ref struct NativeSplit<T> where T : IEquatable<T>
+    {
+        /// <summary>
+        ///     Buffer
+        /// </summary>
+        private readonly ReadOnlySpan<T> _buffer;
+
+        /// <summary>
+        ///     Separator
+        /// </summary>
+        private readonly ReadOnlySpan<T> _separator;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="separator">Separator</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeSplit(ReadOnlySpan<T> buffer, in T separator)
+        {
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer)) == null || buffer.Length == 0)
+                throw new ArgumentNullException(nameof(buffer));
+            if (separator == null)
+                throw new ArgumentNullException(nameof(separator));
+            _buffer = buffer;
+            _separator = MemoryMarshal.CreateReadOnlySpan(ref Unsafe.AsRef(in separator), 1);
+        }
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="buffer">Buffer</param>
+        /// <param name="separator">Separator</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public NativeSplit(ReadOnlySpan<T> buffer, ReadOnlySpan<T> separator)
+        {
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(buffer)) == null || buffer.Length == 0)
+                throw new ArgumentNullException(nameof(buffer));
+            if (Unsafe.AsPointer(ref MemoryMarshal.GetReference(separator)) == null || separator.Length == 0)
+                throw new ArgumentNullException(nameof(separator));
+            _buffer = buffer;
+            _separator = separator;
+        }
+
+        /// <summary>
+        ///     Get enumerator
+        /// </summary>
+        public Enumerator GetEnumerator() => new(_buffer, _separator);
+
+        /// <summary>
+        ///     Enumerator
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public ref struct Enumerator
+        {
+            /// <summary>
+            ///     Current
+            /// </summary>
+            private ReadOnlySpan<T> _current;
+
+            /// <summary>
+            ///     Buffer
+            /// </summary>
+            private ReadOnlySpan<T> _buffer;
+
+            /// <summary>
+            ///     Separator
+            /// </summary>
+            private readonly ReadOnlySpan<T> _separator;
+
+            /// <summary>
+            ///     Structure
+            /// </summary>
+            /// <param name="buffer">Buffer</param>
+            /// <param name="separator">Separator</param>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(ReadOnlySpan<T> buffer, ReadOnlySpan<T> separator)
+            {
+                _current = default;
+                _buffer = buffer;
+                _separator = separator;
+            }
+
+            /// <summary>
+            ///     Move next
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                var index = _separator.Length == 1 ? _buffer.IndexOf(_separator[0]) : _buffer.IndexOf(_separator);
+                if (index < 0)
+                {
+                    if (_buffer.Length > 0)
+                    {
+                        _current = _buffer;
+                        _buffer = default;
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                _current = _buffer.Slice(0, index);
+                _buffer = _buffer.Slice(index + _separator.Length);
+                return true;
+            }
+
+            /// <summary>
+            ///     Current
+            /// </summary>
+            public ReadOnlySpan<T> Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _current;
+            }
+        }
+    }
+}
