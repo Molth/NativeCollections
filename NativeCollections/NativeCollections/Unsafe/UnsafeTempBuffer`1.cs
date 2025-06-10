a0e296c35d8f2e8ccac818c1189438a49636a174
Molth Nevin <qq2201591870@163.com>
2025-06-11 02:47:46 +0800

    TempBuffer

---
+++ NativeCollections/NativeCollections/Unsafe/UnsafeTempBuffer`1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA2208
+#pragma warning disable CA2231
+#pragma warning disable CS8632
+
+// ReSharper disable ALL
+
+namespace NativeCollections
+{
+    /// <summary>
+    ///     Native temp buffer
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    [StructLayout(LayoutKind.Sequential)]
+    [UnsafeCollection(FromType.None)]
+    public readonly unsafe struct UnsafeTempBuffer<T> : IDisposable where T : unmanaged
+    {
+        /// <summary>
+        ///     Array
+        /// </summary>
+        private readonly byte[] _array;
+
+        /// <summary>
+        ///     Length
+        /// </summary>
+        private readonly int _length;
+
+        /// <summary>
+        ///     Is created
+        /// </summary>
+        public bool IsCreated => _array != null;
+
+        /// <summary>
+        ///     Length
+        /// </summary>
+        public int Length => _length;
+
+        /// <summary>
+        ///     Structure
+        /// </summary>
+        /// <param name="length">Length</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UnsafeTempBuffer(int length)
+        {
+            _array = ArrayPool<byte>.Shared.Rent(length * sizeof(T));
+            _length = length;
+        }
+
+        /// <summary>
+        ///     Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() => ArrayPool<byte>.Shared.Return(_array);
+
+        /// <summary>
+        ///     As span
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<T> AsSpan()
+        {
+#if NET5_0_OR_GREATER
+            ref var reference = ref MemoryMarshal.GetArrayDataReference(_array);
+#else
+            ref var reference = ref MemoryMarshal.GetReference(_array.AsSpan());
+#endif
+            return MemoryMarshal.CreateSpan(ref Unsafe.As<byte, T>(ref reference), _length);
+        }
+
+        /// <summary>
+        ///     As readOnly span
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public ReadOnlySpan<T> AsReadOnlySpan()
+        {
+#if NET5_0_OR_GREATER
+            ref var reference = ref MemoryMarshal.GetArrayDataReference(_array);
+#else
+            ref var reference = ref MemoryMarshal.GetReference(_array.AsSpan());
+#endif
+            return MemoryMarshal.CreateReadOnlySpan(ref Unsafe.As<byte, T>(ref reference), _length);
+        }
+
+        /// <summary>
+        ///     Equals
+        /// </summary>
+        /// <returns>Equals</returns>
+        public override bool Equals(object? obj) => throw new NotSupportedException("CannotCallEquals");
+
+        /// <summary>
+        ///     Get hashCode
+        /// </summary>
+        /// <returns>HashCode</returns>
+        public override int GetHashCode() => throw new NotSupportedException("CannotCallGetHashCode");
+
+        /// <summary>
+        ///     To string
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString() => $"UnsafeTempBuffer<{typeof(T).Name}>[{_length}]";
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(ReadOnlySpan<T> buffer)
+        {
+            var temp = new UnsafeTempBuffer<T>(buffer.Length);
+            buffer.CopyTo(temp.AsSpan());
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0)
+        {
+            var temp = new UnsafeTempBuffer<T>(1);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1)
+        {
+            var temp = new UnsafeTempBuffer<T>(2);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2)
+        {
+            var temp = new UnsafeTempBuffer<T>(3);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3)
+        {
+            var temp = new UnsafeTempBuffer<T>(4);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4)
+        {
+            var temp = new UnsafeTempBuffer<T>(5);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5)
+        {
+            var temp = new UnsafeTempBuffer<T>(6);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6)
+        {
+            var temp = new UnsafeTempBuffer<T>(7);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7)
+        {
+            var temp = new UnsafeTempBuffer<T>(8);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8)
+        {
+            var temp = new UnsafeTempBuffer<T>(9);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9)
+        {
+            var temp = new UnsafeTempBuffer<T>(10);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10)
+        {
+            var temp = new UnsafeTempBuffer<T>(11);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10, in T arg11)
+        {
+            var temp = new UnsafeTempBuffer<T>(12);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            buffer[11] = arg11;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10, in T arg11, in T arg12)
+        {
+            var temp = new UnsafeTempBuffer<T>(13);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            buffer[11] = arg11;
+            buffer[12] = arg12;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10, in T arg11, in T arg12, in T arg13)
+        {
+            var temp = new UnsafeTempBuffer<T>(14);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            buffer[11] = arg11;
+            buffer[12] = arg12;
+            buffer[13] = arg13;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10, in T arg11, in T arg12, in T arg13, in T arg14)
+        {
+            var temp = new UnsafeTempBuffer<T>(15);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            buffer[11] = arg11;
+            buffer[12] = arg12;
+            buffer[13] = arg13;
+            buffer[14] = arg14;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Create
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static UnsafeTempBuffer<T> Create(in T arg0, in T arg1, in T arg2, in T arg3, in T arg4, in T arg5, in T arg6, in T arg7, in T arg8, in T arg9, in T arg10, in T arg11, in T arg12, in T arg13, in T arg14, in T arg15)
+        {
+            var temp = new UnsafeTempBuffer<T>(16);
+            var buffer = temp.AsSpan();
+            buffer[0] = arg0;
+            buffer[1] = arg1;
+            buffer[2] = arg2;
+            buffer[3] = arg3;
+            buffer[4] = arg4;
+            buffer[5] = arg5;
+            buffer[6] = arg6;
+            buffer[7] = arg7;
+            buffer[8] = arg8;
+            buffer[9] = arg9;
+            buffer[10] = arg10;
+            buffer[11] = arg11;
+            buffer[12] = arg12;
+            buffer[13] = arg13;
+            buffer[14] = arg14;
+            buffer[15] = arg15;
+            return temp;
+        }
+
+        /// <summary>
+        ///     Empty
+        /// </summary>
+        public static UnsafeTempBuffer<T> Empty => new();
+    }
+}
